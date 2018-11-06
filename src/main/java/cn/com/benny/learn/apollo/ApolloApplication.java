a4d41eb669b52a66a8307ec55ff2cb8a187823d8
benny <178542285@qq.com>
2018-11-06 20:45:29 +0800

    添加apollo的使用

---
+++ src/main/java/cn/com/benny/learn/apollo/ApolloApplication.java
@@ -0,0 +1,30 @@
+/*
+ * CopyRight benny
+ * ProjectName: java-learn-code
+ * Author: benny
+ * Date: 18-11-6 下午7:57
+ * LastModified: 18-11-6 下午7:57
+ */
+
+package cn.com.benny.learn.apollo;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Description:<p>
+ * QQ: 178542285
+ *
+ * @Author benny
+ * @Date: 2018/11/6
+ * @Time: 19:57
+ */
+@SpringBootApplication
+@EnableApolloConfig
+public class ApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloApplication.class,args);
+    }
+}
