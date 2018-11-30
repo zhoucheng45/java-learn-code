f1a50a12bbf0218a024ad25a3910cb4e7c188151
benny <178542285@qq.com>
2018-11-30 18:55:38 +0800

    添加log4j2和logback使用

--- log4j2-learn/src/main/java/cn/com/benny/learn/controller/AController.java
+++ log4j2-learn/src/main/java/cn/com/benny/learn/controller/AController.java
@@ -12,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * <p>Description:<p>
  * QQ: 178542285
@@ -25,10 +28,32 @@ public class AController {
 
     Logger logger = LoggerFactory.getLogger(AController.class);
 
-    @RequestMapping("/a")
-    public @ResponseBody String a(@RequestParam("name") String name){
-        logger.info("收到请求，参数{}", name);
+    private CountDownLatch cd = new CountDownLatch(50);
 
+    @RequestMapping("/a")
+    public @ResponseBody String a(@RequestParam("name") String name) throws InterruptedException {
+        Date date = new Date();
+//        for (int i=0; i<10000*10000;i++){
+//            logger.info("收到请求，参数{}", name);
+//            if(i%10000 == 0){
+//                System.out.println("请求次数"+i);
+//            }
+//        }
+
+        for (int j=0; j<50; j++){
+            new Thread(() -> {
+                for (int i=0; i<500*100;i++) {
+                    logger.info("收到请求，参数{}", name);
+                }
+                cd.countDown();
+            }).start();
+        }
+
+        cd.await();
+
+
+        Date last = new Date();
+        logger.info("耗时{}}", last.getTime() - date.getTime());
         return name;
 
     }
